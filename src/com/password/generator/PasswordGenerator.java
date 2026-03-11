136fc572cf89d919d1193cf112b274425f7791b1
shaaravbaruah-bit <shaaravbaruah@gmail.com>
2026-03-12 02:42:19 +0530

    Added multiple password generation and strength checker

--- src/com/password/generator/PasswordGenerator.java
+++ src/com/password/generator/PasswordGenerator.java
@@ -19,11 +19,70 @@ public class PasswordGenerator {
             int choice = sc.nextInt();
 
             if(choice == 1) {
-                System.out.println("Password generation feature coming soon...");
+
+                System.out.print("Enter password length: ");
+                int length = sc.nextInt();
+
+                System.out.print("How many passwords generate: ");
+                int count = sc.nextInt();
+
+                String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+
+                for(int j = 1; j <= count; j++) {
+
+                    String password = "";
+
+                    for(int i = 0; i < length; i++) {
+
+                        int index = (int)(Math.random() * chars.length());
+
+                        password += chars.charAt(index);
+
+                    }
+
+                    System.out.println("Password " + j + ": " + password);
+                }
             }
 
             else if(choice == 2) {
-                System.out.println("Password strength checker coming soon...");
+
+                System.out.print("Enter password to check: ");
+                String password = sc.next();
+
+                int length = password.length();
+
+                boolean hasUpper = false;
+                boolean hasNumber = false;
+                boolean hasSymbol = false;
+
+                for(int i = 0; i < length; i++) {
+
+                    char ch = password.charAt(i);
+
+                    if(Character.isUpperCase(ch)) {
+                        hasUpper = true;
+                    }
+
+                    else if(Character.isDigit(ch)) {
+                        hasNumber = true;
+                    }
+
+                    else if(!Character.isLetterOrDigit(ch)) {
+                        hasSymbol = true;
+                    }
+                }
+
+                if(length >= 8 && hasUpper && hasNumber && hasSymbol) {
+                    System.out.println("Password Strength: Strong");
+                }
+
+                else if(length >= 6) {
+                    System.out.println("Password Strength: Medium");
+                }
+
+                else {
+                    System.out.println("Password Strength: Weak");
+                }
             }
 
             else if(choice == 3) {
@@ -38,5 +97,6 @@ public class PasswordGenerator {
             System.out.println();
         }
 
+        sc.close();
     }
 }
